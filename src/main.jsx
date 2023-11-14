8c6ffacc4864e3ed532eefc54f610ae8f4df97cd
Sean Worrell <spworrell@gmail.com>
2023-11-14 16:47:53 -0500

    files moved and setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import App from './components/App.jsx'
+// import Footer from './components/footer'
+import Header from './components/header.jsx'
+
 import './index.css'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <Header />
+  
   </React.StrictMode>,
 )
