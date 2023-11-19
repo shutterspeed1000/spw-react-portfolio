a45f3dc2929d48b6fd1cadf0b72d6d96f2759635
Sean Worrell <spworrell@gmail.com>
2023-11-18 21:24:20 -0500

    footer

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,18 +3,32 @@ export default function About() {
     <div class="about">
       <h1>About Sean</h1>
       <p>
-        <img src="./Sean.jpg" class="headshot" align="left"></img>
-        Sean Worrell is an experienced system administrator and application
-        developer with a strong track record of success. With expertise in
-        managing complex systems and developing innovative applications, Sean
-        delivers outstanding results. His portfolio showcases his skills in
-        system administration, application development, and problem-solving,
-        highlighting his ability to optimize IT infrastructures and drive
-        impactful projects. As a Six Sigma Blackbelt, Sean can help identify
-        issues and create resolutions that correct the root cause. As a trainied
-        MERN developer, Sean can bring you custom solitions the fit your
-        requierments. Sean's versatility, technical proficiency, and commitment
-        to excellence make him an invaluable asset to any organization.
+        <img src="./Sean.jpg" class="headshot" align="right"></img>
+        Sean Worrell is a highly experienced professional in the domains of
+        system administration and application development, distinguished by a
+        robust history of success. Possessing profound expertise, Sean adeptly
+        navigates the intricacies of managing complex systems and crafting
+        innovative applications, consistently delivering outstanding outcomes.
+      </p>
+      <p>
+        His comprehensive portfolio serves as a compelling showcase of his
+        proficiency in system administration, application development, and
+        strategic problem-solving. Notably, Sean excels in optimizing IT
+        infrastructures and spearheading impactful projects, reflecting his
+        commitment to driving excellence.
+      </p>
+      <p>
+        As a Six Sigma Blackbelt, Sean brings a rigorous and systematic approach
+        to issue identification and resolution, addressing root causes with
+        precision. Additionally, his credentials as a trained MERN developer
+        underscore his capability to provide bespoke solutions that align
+        seamlessly with specific requirements.
+      </p>
+      <p>
+        Sean's versatility, technical prowess, and unwavering dedication to
+        excellence position him as an invaluable asset to any discerning
+        organization seeking a seasoned professional with a proven track record
+        of delivering high-quality results.
       </p>
     </div>
   );
