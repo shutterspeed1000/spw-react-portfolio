ad6f59dab566e8edb937ab80637925f087fcaf48
Sean Worrell <spworrell@gmail.com>
2023-11-18 20:35:20 -0500

    fixed

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,15 +1,20 @@
 export default function About() {
-    return (
-      <div class="about">
-        <h1>About Sean</h1>
-        <p>
-        Sean Worrell is an experienced system administrator and application developer with a strong
-        track record of success. With expertise in managing complex systems and developing innovative applications, Sean
-        delivers outstanding results. His portfolio showcases his skills in system administration, application
-        development, and problem-solving, highlighting his ability to optimize IT infrastructures and drive impactful
-        projects. Sean's versatility, technical proficiency, and commitment to excellence make him an invaluable asset
-        to any organization.
-        </p>
-      </div>
-    );
-  }
+  return (
+    <div class="about">
+      <h1>About Sean</h1>
+      <p>
+        Sean Worrell is an experienced system administrator and application
+        developer with a strong track record of success. With expertise in
+        managing complex systems and developing innovative applications, Sean
+        delivers outstanding results. His portfolio showcases his skills in
+        system administration, application development, and problem-solving,
+        highlighting his ability to optimize IT infrastructures and drive
+        impactful projects. As a Six Sigma Blackbelt, Sean can help identify
+        issues and create resolutions that correct the root cause. As a trainied
+        MERN developer, Sean can bring you custom solitions the fit your
+        requierments. Sean's versatility, technical proficiency, and commitment
+        to excellence make him an invaluable asset to any organization.
+      </p>
+    </div>
+  );
+}
